f162648c4b3a4f69883689e143c9b3d0d88a6171
Pavan Wagh <138033700+pavanwagh1@users.noreply.github.com>
2024-08-06 18:34:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+function App() {
+  // let a = 6;
+  
+  return (
+    <>
+    {/* Css */}
+    <h3 className="main">
+      I am applying the Css
+    </h3>
+
+    {/* Inline CSS : */}
+    <span style={{color:'red', margin:'100px'}}>Pavan</span>
+
+    </>
+    
+    
+
+
+    // For accessing variables : 
+    // <h2>The value of a = {a + 2}</h2>
+
+
+    // This is also Ok
+    // <>
+    // <h1>I am Pavan. Created new Component</h1>
+    //   <span>I am web developer..</span>
+    // </>
+
+  );
+}
+export default App;
